9fb9bc7f3be0861f50f686902c0145717a0a3bd0
tmarek <tiago.marek@adp.com>
2023-10-27 14:50:42 -0300

    add lua to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
     vim.defer_fn(function()
       require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { 'tsx', 'javascript', 'typescript' },
+        ensure_installed = { 'tsx', 'javascript', 'typescript', 'lua' },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
         auto_install = true,
